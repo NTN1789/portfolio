90bdd60cf2edcf64890a6a273526d40e6f673817
NTN1789 <natanalmeida040@gmail.com>
2023-01-29 14:47:54 -0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import * as S from "./header/header"
+import { GlobalStyle } from "../style/global"
+export default  function  ()  {
+    return(
+        <>
+        <GlobalStyle/>
+      <S.banner>
+        <ul>
+          <li>Home</li>
+          <li> sobre</li>
+          <li> contatos </li>
+          <li> projetos </li>
+        </ul>
+
+
+
+
+
+
+      </S.banner>
+        
+
+        </>
+    )
+}
+
---
+++ src/components/header/header.js
@@ -0,0 +1,31 @@
+import styled from "styled-components"
+
+export const banner = styled.header`
+    display:flex;
+    justify-content:center;
+    padding: 10px 60px ;
+
+    
+    height:12vh;
+    background-color:tomato;
+
+    ul{
+        display:flex;
+        align-items:center;
+        
+    }
+
+    li{
+    text-decoration:none;
+    color:white;
+    font-size:1.5rem;
+    font-weight:500 ;
+    padding: 10px 15px ;
+    text-transform: uppercase;
+    list-style:none;
+    }
+
+
+
+
+`
