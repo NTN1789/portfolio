fb527d44af2305d9e34ffbcff9fcc87e26163cd2
NTN1789 <natanalmeida040@gmail.com>
2023-04-23 23:08:38 -0300

    arruamndo contatos

--- src/components/Contatos/Contatos.jsx
+++ src/components/Contatos/Contatos.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import { Form } from "./StylesContato"
+import "./Contato.css"
 export const Contatos = () => {
   return (
     <div>
-           <form>
+     
+
+           <Form>
+           <p>Entre em contato </p>
     
     <label>
         <span> Email:</span>
@@ -18,19 +22,85 @@ export const Contatos = () => {
     </label>
 
     <label>
-        <span> Senha:</span>
+        <span> Nome:</span>
         <input
-         type="password" 
-         name='Password' 
+         type="name" 
+         name='nome' 
          required
-         placeholder='Senha do usuário'
+         placeholder='nome do usuário'
          />
     </label>
 
+    <label>
+        <span> Telefone:</span>
+        <input
+         type="nome" 
+         name='number' 
+         required
+         placeholder='Telefone do usuário'
+         />
+    </label>
+
+    <label for=""> deixe sua Mensagem</label>
+                        <textarea rows="4"></textarea>
+                        <button type="submit" id="formId">Sua mensagem </button>
 
  
-</form>
-        
+    </Form>
+    <section id="contatos">
+      <div className="container-contatos">
+        <div className="contatos">
+          <div className="icons">
+            <a href="#">
+              <div className="layer">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span className="fab fa-github-f"><i class="bi bi-github"></i>
+                </span>
+              </div>
+              <div className="text">GitHub</div>
+            </a>
+            <a href="#">
+              <div className="layer">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span className="fab fa-linkedin-in"><i class="bi bi-linkedin"></i>
+                </span>
+              </div>
+              <div className="text">Linkedin</div>
+            </a>
+            <a href="#">
+              <div className="layer">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span className="fab fa-gmail-f"><i class="bi bi-gmail"><i class="bi bi-envelope-fill"></i>
+                </i>
+                </span>
+              </div>
+              <div className="text">Gmail</div>
+            </a>
+            <a href="#">
+              <div className="layer">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span className="fab fa-twitter-f"><i class="bi bi-twitter"></i>
+                </span>
+              </div>
+              <div className="text">Twitter</div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+      
         </div>
   )
 }
---
+++ src/components/Contatos/StylesContato.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const Form = styled.form`
+display:flex;
+justify-content:center;
+align-items:center;
+flex-direction: column;
+padding:62px;
+
+
+p{
+    text-align:center;
+    font-size:1.9em;
+    padding:10px;
+   
+    height:12vh;
+}
+
+
+
+
+
+
+`
